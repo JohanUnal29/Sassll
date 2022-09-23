496127b980508f1e5f8b7688bb15500e200a644e
Nicolás Ardila <johan.ardilah@gmail.com>
2022-09-23 14:44:17 -0500

    testeando formulario

--- php/form.php
+++ php/form.php
@@ -1,13 +1,15 @@
 <?php
 
-$nombre = $_POST["nombre"];
-$apellido = $_POST["apellido"];
-$mail = $_POST["email"];
-$numero = $_POST["numero"];
-$direccion = $_POST["direccion"];
-$pais = $_POST["pais"];
-$ciudad = $_POST["ciudad"];
-$departamento = $_POST["departamento"];
+$nombre = $_POST['nombre'];
+$apellido = $_POST['apellido'];
+$mail = $_POST['email'];
+$numero = $_POST['numero'];
+$direccion = $_POST['direccion'];
+$pais = $_POST['pais'];
+$ciudad = $_POST['ciudad'];
+$departamento = $_POST['departamento'];
+
+$header .= "Content-Type: text/plain";
 
 $mensaje = "Este mensaje fue enviado por ". $nombre .",\r\n";
 $mensaje .= "Su e-mail es: ". $mail ." \r\n";
@@ -16,14 +18,14 @@ $mensaje .= "Su dirección es: ". $direccion ." \r\n";
 $mensaje .= "País: ". $pais ." \r\n";
 $mensaje .= "Su ciudad es: ". $ciudad ." \r\n";
 $mensaje .= "Su departamento es: ". $departamento ." \r\n";
-$mensaje .= "Enviado el: ". date("d/m/y", time());
+$mensaje .= "Enviado el: ". date('d/m/y', time());
 
-$para = "johan.ardilah@gmail.com";
-$asunto = "Este mail fue enviado desde la pagina Daseinjoyeria.com";
+$para = 'johan.ardilah@gmail.com';
+$asunto = 'Este mail fue enviado desde la pagina Daseinjoyeria.com';
 
 mail($para, $asunto, utf8_decode($mensaje), $header);
 
-header("Location: ../index.html");
+header('Location:../pages/agradecimiento.html');
 
 ?>
 
